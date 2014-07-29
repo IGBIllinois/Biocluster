338f8d95df785b49eb0c00209535bfde675b6ce9
bio-boris <bsadkhin@gmail.com>
2014-07-29 13:43:15 -0500

    Create release_jobs_from_hold.py
    Unchecked version.
    A way to release a job array from hold.

---
+++ release_jobs_from_hold.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+use strict;
+use Getopt::Long;
+
+
+my $usage    = "\t\t --usage $0 job_id=<PBS JOB ID> min=<Start of array> max=<End of array>\n";
+my $command  = "releasehold -a";
+my $job_id;
+my $min;
+my $max;
+$\= "\n";
+
+
+
+if (! scalar(@ARGV)  ) {
+        die $usage . scalar @ARGV;
+}
+
+GetOptions ("job_id=i"  => \$job_id,
+              "min=i"   => \$min,
+              "max=i"   => \$max)   ;
+
+if(! $job_id || ! $min || ! $max){
+        die $usage;
+}
+
+
+print "job=$job_id min=$min max=$max\n";
+
+
+for(my $i = $min; $i < $max; $i++){
+        my $call = "$command \"$job_id" . '\[' . $i . '\]"';
+        print $call;
+        system($call);
+}
